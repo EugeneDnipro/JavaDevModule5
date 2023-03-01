3a38685a343dcabc59b069aad02a9425b9f1ae2d
EugeneDnipro <eugenednepr@gmail.com>
2023-03-01 22:45:13 +0200

    Changes in DatabasePopulateService class

--- src/main/java/org/example/DatabasePopulateService.java
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -11,6 +11,11 @@ public class DatabasePopulateService {
     private static final String POPULATE_DB_FILE = "sql/populate_db.sql";
 
     public static void main(String[] args) throws SQLException {
+        String workerTableSqlTemplate = "INSERT INTO worker (id, name, birthday, level, salary) VALUES (?);";
+        String clientTableSqlTemplate = "INSERT INTO client (id, name) VALUES (?);";
+        String projectTableSqlTemplate = "INSERT INTO project (id, client_id, start_date, finish_date) VALUES (?);";
+        String project_workerTableSqlTemplate = "INSERT INTO project_worker (project_id, worker_id) VALUES (?);";
+
         try (InputStream fis = new FileInputStream(POPULATE_DB_FILE);
              Scanner scanner = new Scanner(fis);
              Statement stmt = Database.getInstance().getConnection().createStatement()) {
